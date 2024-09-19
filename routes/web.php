400eaf4ceedea8a8b29f92cfc87f97733c16264d
interdate <contact@interdate-ltd.co.il>
2024-09-19 16:10:37 +0300

    Add post table

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PostController::class, 'index']);
+//Route::resource('/', PostController::class)->names(
+//    [
+//        'index'=>'posts.index',
+//        'create' => 'posts.create',
+//        'store' => 'posts.store',
+//        'show' => 'posts.show'
+//    ]
+//);
